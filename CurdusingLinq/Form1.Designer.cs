8b7bddc468fab1933b2a9337965acdf4445d4b7c
Arunava Banerjee <Arunava Banerjee@DESKTOP-RBAIMPE>
2020-12-02 04:54:06 -0800

    Add project files.

---
+++ CurdusingLinq/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace CurdusingLinq
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.idbox = new System.Windows.Forms.TextBox();
+            this.namebox = new System.Windows.Forms.TextBox();
+            this.roombox = new System.Windows.Forms.TextBox();
+            this.citybox = new System.Windows.Forms.TextBox();
+            this.update = new System.Windows.Forms.Button();
+            this.insert = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(204, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(347, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RESTAURANT DETAILS ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(336, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(336, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "NAME";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(336, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ROOMS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(336, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "CITY";
+            // 
+            // idbox
+            // 
+            this.idbox.Location = new System.Drawing.Point(463, 96);
+            this.idbox.Name = "idbox";
+            this.idbox.Size = new System.Drawing.Size(100, 20);
+            this.idbox.TabIndex = 5;
+            // 
+            // namebox
+            // 
+            this.namebox.Location = new System.Drawing.Point(463, 138);
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(100, 20);
+            this.namebox.TabIndex = 5;
+            // 
+            // roombox
+            // 
+            this.roombox.Location = new System.Drawing.Point(463, 174);
+            this.roombox.Name = "roombox";
+            this.roombox.Size = new System.Drawing.Size(100, 20);
+            this.roombox.TabIndex = 5;
+            // 
+            // citybox
+            // 
+            this.citybox.Location = new System.Drawing.Point(463, 209);
+            this.citybox.Name = "citybox";
+            this.citybox.Size = new System.Drawing.Size(100, 20);
+            this.citybox.TabIndex = 5;
+            // 
+            // update
+            // 
+            this.update.Location = new System.Drawing.Point(263, 311);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(75, 23);
+            this.update.TabIndex = 6;
+            this.update.Text = "UPADTE";
+            this.update.UseVisualStyleBackColor = true;
+            // 
+            // insert
+            // 
+            this.insert.Location = new System.Drawing.Point(69, 310);
+            this.insert.Name = "insert";
+            this.insert.Size = new System.Drawing.Size(75, 23);
+            this.insert.TabIndex = 7;
+            this.insert.Text = "INSERT";
+            this.insert.UseVisualStyleBackColor = true;
+            this.insert.Click += new System.EventHandler(this.insert_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(447, 309);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(677, 309);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.insert);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.citybox);
+            this.Controls.Add(this.roombox);
+            this.Controls.Add(this.namebox);
+            this.Controls.Add(this.idbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox idbox;
+        private System.Windows.Forms.TextBox namebox;
+        private System.Windows.Forms.TextBox roombox;
+        private System.Windows.Forms.TextBox citybox;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button insert;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ CurdusingLinq/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurdusingLinq
+{
+    public partial class Form1 : Form
+    {
+
+        RestaurantDBDataContext db;
+
+        Restaurant r;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insert_Click(object sender, EventArgs e)
+        {
+
+            db = new RestaurantDBDataContext();
+
+            r = new Restaurant();
+
+            r.id = int.Parse(idbox.Text);
+            r.name = namebox.Text;
+            r.rooms = int.Parse(roombox.Text);
+            r.city = citybox.Text;
+
+            db.Restaurants.InsertOnSubmit(r);
+            db.SubmitChanges();
+
+
+        }
+    }
+}
